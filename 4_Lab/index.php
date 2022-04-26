38cb7ca892ed43c0ab66d86dad6c775551fbf241
alicekaeva <57089815+alicekaeva@users.noreply.github.com>
2022-04-27 00:59:35 +1000

    added lab 4

---
+++ 4_Lab/index.php
@@ -0,0 +1,42 @@
+<form action="/" method="post">
+    <label> Логин
+        <input name="login" autocomplete="off"/>
+    </label><br>
+    <label> Пароль
+        <input type="password" name="password" autocomplete="off"/>
+    </label><br>
+    <label> Сообщение
+        <input name="message" autocomplete="off"/>
+    </label><br>
+    <input type="submit" value="Опубликовать" autocomplete="off"/>
+</form>
+
+<?php
+
+function addMessage($login, $message){
+    $myJSON = json_decode(file_get_contents("messages.json"));
+    $messageBody = (object) ['date' => time(), 'user' => $login, 'message' => $message];
+    $myJSON->messages[] = $messageBody;
+    file_put_contents("messages.json", json_encode($myJSON));
+}
+
+function displayMessages(){
+    $myJSON = json_decode(file_get_contents("messages.json"));
+    foreach($myJSON->messages as $message){
+        echo '<p style="color:blue">' . date('m/d/Y H:i:s', $message->date) . ' ' . $message->user . '</p>';
+        echo '<p>' . $message->message . '</p>';
+    }
+}
+
+$login = $_POST["login"];
+$password = $_POST["password"];
+$message = $_POST["message"];
+if (($login === "alicekaeva" && $password === "crazy_frog_2022") || ($login === "шлепа" && $password === "чмоня")) {
+    addMessage($login, $message);
+}
+else {
+    echo '<p style="color:red">Такого пользователя не существует</p>';
+}
+displayMessages();
+
+?>
